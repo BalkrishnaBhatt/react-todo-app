96da34f7f3753f7109f2897ac1a77c128183dff4
Balkrishna Bhatt <BalkrishnaBhatt@MacBook-Pro.local>
2021-06-27 19:34:34 +0530

    Updates

---
+++ precache-manifest.44f32495fb0311ee8fb5780abea4694e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de7b89a5585f614c50d7378c3b29f56",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "f079f723602d3287ddd9",
+    "url": "/react-todo-app/static/css/2.88074101.chunk.css"
+  },
+  {
+    "revision": "26a6b10b0f7468eeaff3",
+    "url": "/react-todo-app/static/css/main.9ae135fd.chunk.css"
+  },
+  {
+    "revision": "f079f723602d3287ddd9",
+    "url": "/react-todo-app/static/js/2.f12a25bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-app/static/js/2.f12a25bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a6b10b0f7468eeaff3",
+    "url": "/react-todo-app/static/js/main.97a57193.chunk.js"
+  },
+  {
+    "revision": "a02306a5582400e4c981",
+    "url": "/react-todo-app/static/js/runtime-main.15b7a15e.js"
+  }
+]);
